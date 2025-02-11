700842332b4e761900778e8c16d426f92e43f7ca
A1909bek <aa2004bek@gmail.com>
2025-02-11 07:47:31 +0000

    next commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,8 +5,10 @@ from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
+        fields = ['username','email','age']
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
+        fields = ['username','email','age']
