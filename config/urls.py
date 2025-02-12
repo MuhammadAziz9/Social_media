a903b31ec8bb9ac0c355d10a11be29b90ef26170
A1909bek <aa2004bek@gmail.com>
2025-02-12 17:43:48 +0000

    next commit

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
     path('posts/',include('posts.urls')),
     path('',LoginView.as_view(template_name='registration/login.html'),name='login'),
+    path('',LoginView.as_view(),name='logout'),
 ]
 
 if settings.DEBUG:
