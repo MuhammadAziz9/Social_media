e1dd67a5fb058c400ba9af7f0cd05e50b85c910a
A1909bek <aa2004bek@gmail.com>
2025-02-13 15:01:26 +0000

    next commit

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('like/<int:post_id>/',views.toggle_like,name='toggle_like'),
     path('post/<int:post_id>/',views.post_detail,name='post_detail'),
     path('post/<int:post_id>/share',views.toggle_share,name='share_post'),
+    path('post/<int:post_id>/save',views.toggle_save,name='toggle_save'),
+    path('saved-post/',views.saved_post,name='saved_post'),
+    path('delete-saved-post/<int:saved_post_id>/',views.delete_saved_post,name='delete_saved_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Post,Comment,Like,Share,Save,Friendship
-from django.http import HttpResponse
-from django.db import models
-from accounts.models import CustomUser
+from .models import Post,Comment,Like,Share,Save
 from .forms import CommentForm
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def home(request):
@@ -14,7 +12,9 @@ def home(request):
     }
     return render(request,'home.html',context)
 
-from django.http import JsonResponse
+@login_required
+
+
 
 @login_required
 def toggle_like(request, post_id):
@@ -27,12 +27,12 @@ def toggle_like(request, post_id):
         Like.objects.create(user=request.user, post=post)
 
 
-    return redirect('post_detail',post_id=post.id)
+    return redirect('home')
 
 def toggle_share(request,post_id):
     post = get_object_or_404(Post,id=post_id)
     Share.objects.create(post=post,user=request.user)
-    return redirect('post_detail',post_id=post.id)
+    return redirect('home')
 
 def post_detail(request,post_id):
     post = get_object_or_404(Post,id=post_id)
@@ -53,6 +53,28 @@ def post_detail(request,post_id):
         'post':post
     }
     return render(request,'post_detail.html',context)
+
+def toggle_save(request,post_id):
+    post =get_object_or_404(Post,id=post_id)
+    saved_post,created = Save.objects.get_or_create(user=request.user,post=post)
+
+    if not created:
+        saved_post.delete()
+    
+    return redirect('home')
+
+def saved_post(request):
+    saved_posts = Save.objects.filter(user=request.user).select_related('post')
+    context = {
+        'saved_posts':saved_posts
+    }
+    return render(request,'saved_post.html',context=context)
+
+def delete_saved_post(request,saved_post_id):
+    saved_post = get_object_or_404(Save,id=saved_post_id,user=request.user)
+    saved_post.delete()
+    return redirect('saved_post')
+
 
 
 
