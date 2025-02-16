efe800479fe11aa8008145239baa095cb4942293
A1909bek <aa2004bek@gmail.com>
2025-02-16 01:21:37 +0000

    next commit

--- group/urls.py
+++ group/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('group_list/',views.group_list,name='group_list'),
-    path('create_group/',views.create_group,name='create_group')
+    path('create_group/',views.create_group,name='create_group'),
+    path('join/<int:group_id>/',views.join_group,name='join_group'),
+    path('leave/<int:group_id>/',views.leave_group,name='leave_group'),
 ]
--- group/views.py
+++ group/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Group,GroupMember
 from.forms import GroupForm
 
@@ -26,3 +26,14 @@ def create_group(request):
         'form':form,
     }
     return render(request,'group/create_group.html',context=context)
+
+def join_group(request,group_id):
+    group = get_object_or_404(Group,id=group_id)
+    if not GroupMember.objects.filter(user=request.user,group=group).exists():
+        GroupMember.objects.create(group=group,user=request.user)
+    return redirect('group_list')
+
+def leave_group(request,group_id):
+    group = get_object_or_404(Group,id=group_id)
+    GroupMember.objects.filter(group=group,user=request.user).delete()
+    return redirect('group_list')
